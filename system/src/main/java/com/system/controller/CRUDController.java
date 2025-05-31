955ac2992908bf7344c599a6cac07b11ac3883d7
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-05-31 16:02:37 +0530

    Update CRUDController.java

--- system/src/main/java/com/system/controller/CRUDController.java
+++ system/src/main/java/com/system/controller/CRUDController.java
@@ -3,14 +3,16 @@ package com.system.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.system.model.Student;
-
+@RestController
 public class CRUDController<T, M> {
 	private Map<T, M> objects = new HashMap<T, M>();
 
@@ -21,20 +23,32 @@ public class CRUDController<T, M> {
 	public void setObjects(Map<T, M> objects) {
 		this.objects = objects;
 	}
-	
+
 	@GetMapping("/")
 	public Map<T, M> getAll() {
 		return objects;
 	}
-	
+
 	@GetMapping("/{id}")
 	public M getById(@PathVariable("id") T id) {
 		return objects.get(id);
 	}
 	
 	@PostMapping("/")
-	public String add(@RequestBody M object, @RequestParam T id) {
+	public String add(@RequestBody M object) {
+		objects.put(null, object);
+		return "Object added";
+	}
+
+	@PutMapping("/{id}")
+	public String update(@PathVariable("id") T id, @RequestBody M object) {
 		objects.put(id, object);
-		return "Object added successfully";
+		return "Object updated";
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") T id) {
+		objects.remove(id);
+		return "Object deleted";
 	}
 }
